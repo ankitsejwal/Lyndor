0e185ff8735d1372b3f1c9c6daee2fc799f66105
ankitsejwal <ankit@sejw.al>
2017-08-31 11:29:47 +1000

    create Bulk Download.txt

--- install.py
+++ install.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import message
+import shutil
+from colorama import *
 
 real_path = os.path.realpath(__file__)
 real_path = real_path[:real_path.find('install.py')]
@@ -38,9 +41,9 @@ def create_folder():
     ''' Create lynda folder '''
     if not os.path.exists('Lynda'):
         os.makedirs('Lynda')
-        print '>>> Your Lynda videos will be saved at -> '+ set_path() +'/Lynda (folder)\n'
+        message.print_line('>>> Your Lynda videos will be saved at -> '+ set_path() +'/Lynda (folder)\n')
     else:
-        print '\nLynda folder already exists\n'
+        message.print_line('\nLynda folder already exists\n')
 
 def create_aliases():
     '''Create aliases file'''
@@ -50,11 +53,15 @@ def create_aliases():
         alias = open('aliases.bat', 'w')
         alias.write(run_path)
         alias.close()
-        print '-> aliases.bat file created.\n'
+        message.print_line('-> aliases.bat file created.\n')
 
-def create_lynda_bat():
+def lynda_folder_files():
     ''' create lynda.bat in windows '''
     os.chdir(RESET_PATH)
+    bulk_download = open('Bulk Download.txt','w')
+    bulk_download.close()
+    shutil.move('Bulk Download.txt', read_location_file()+'/Bulk Download.txt')
+    message.print_line('-> Bulk Download.txt file created successfully.\n')
     if check_os() == 'windows':
         run_path = 'python "'+os.getcwd()+'/run.py"'
         lynda = open('Lynda.bat', 'a')
@@ -64,16 +71,22 @@ def create_lynda_bat():
         lynda.writelines(run_path+'\n')
         lynda.writelines('pause')
         lynda.close()
-        print '-> Lynda.bat file created.\n'
+        message.print_line('-> Lynda.bat file created.\n')
         os.rename('Lynda.bat', read_location_file() + '/Lynda.bat')
 
 def create_location_file():
     '''create file that tells the program where to save the new course'''
     os.chdir(RESET_PATH)
+    files = os.listdir(os.getcwd())
+    for content in files:
+        if content == 'location.txt':
+            #Prevent existing location.txt file from overwriting
+            message.print_line('location.txt file already exists, will use the same path inside it for downloads\n')
+            return
     loc_file = open('location.txt', 'w')
     loc_file.write(set_path()+'/Lynda')
     loc_file.close()
-    print '-> location.txt file created.\n'
+    message.print_line('-> location.txt file created.\n')
 
 def read_location_file():
     '''read the content of location.txt'''
@@ -91,7 +104,9 @@ def install_dependencies():
     for module in requirements:
         os.system('pip install '+ module)
     requirements.close()
-    print '\n>>> All the required softwares are installed\n'
+    message.print_line('\n>>> All the required softwares are installed\n')
+    message.colored_message(Fore.LIGHTGREEN_EX, 'If you wish to download your courses to some other FOLDER,'+
+    ' paste the folder path in location.txt file\n')
 
 if __name__ == '__main__':
     try:
@@ -99,11 +114,12 @@ if __name__ == '__main__':
         create_folder()
         create_aliases()
         create_location_file()
-        create_lynda_bat()
+        lynda_folder_files()
         install_dependencies()
     except KeyboardInterrupt:
-        print "Program execution cancelled through keyboard!"
+        message.print_line("Program execution cancelled through keyboard!")
         try:
             sys.exit(0)
         except:
             os._exit(0)
+            
