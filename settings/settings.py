1e44103bbe661aa6124c655a7df6651a566472c2
ankitsejwal <ankit@sejw.al>
2018-03-27 20:13:59 +1100

    flask app

--- settings/settings.py
+++ settings/settings.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def settings():
+def home():
     return render_template('settings.html')
 
-app.run(port=5000, debug=True)
+@app.route('/test')
+def test():
+    with open('./settings/settings.json', 'r') as data:
+        data = json.load(data)
+        return data['preferences']
+app.run(port=5000, debug=True)                                     
