78293df4674a497bee8ce94dd35490378bf8444a
ankitsejwal <ankit@sejw.al>
2018-04-11 23:35:14 +1000

    Bug fix

--- read.py
+++ read.py
@@ -22,21 +22,24 @@ def settings_json(section, *args):
     except:
         return data[section][args[0]]
 
-# read credentials
-username                = settings_json('credentials', 'regular_login', 'username')
-password                = settings_json('credentials', 'regular_login', 'password')
-card_number             = settings_json('credentials', 'library_login', 'card_number')
-card_pin                = settings_json('credentials', 'library_login', 'card_pin')
-organization_url        = settings_json('credentials', 'library_login', "organization_url")
-course_download_pref    = settings_json('credentials', 'course_download_pref')[0]
-exfile_download_pref    = settings_json('credentials', 'exfile_download_pref')[0]
-# read preferences
-location                = settings_json('preferences', 'location')
-download_subtitles      = settings_json('preferences', 'web_browser_for_exfile')[0]
-download_exercise_file  = settings_json('preferences', 'download_exercise_file')
-web_browser_for_exfile  = settings_json('preferences', 'web_browser_for_exfile')[0]
-external_downloader     = settings_json('preferences', 'ext-downloader-aria2-installed')
-download_time           = settings_json('preferences', 'download_time')
-redownload_course       = settings_json('preferences', 'redownload_course')[0]
-# read dependencies
-dependencies            = settings_json('requirements', 'dependencies')
+try:
+    # read credentials
+    username                = settings_json('credentials', 'regular_login', 'username')
+    password                = settings_json('credentials', 'regular_login', 'password')
+    card_number             = settings_json('credentials', 'library_login', 'card_number')
+    card_pin                = settings_json('credentials', 'library_login', 'card_pin')
+    organization_url        = settings_json('credentials', 'library_login', "organization_url")
+    course_download_pref    = settings_json('credentials', 'course_download_pref')[0]
+    exfile_download_pref    = settings_json('credentials', 'exfile_download_pref')[0]
+    # read preferences
+    location                = settings_json('preferences', 'location')
+    download_subtitles      = settings_json('preferences', 'web_browser_for_exfile')[0]
+    download_exercise_file  = settings_json('preferences', 'download_exercise_file')
+    web_browser_for_exfile  = settings_json('preferences', 'web_browser_for_exfile')[0]
+    external_downloader     = settings_json('preferences', 'ext-downloader-aria2-installed')
+    download_time           = settings_json('preferences', 'download_time')
+    redownload_course       = settings_json('preferences', 'redownload_course')[0]
+    # read dependencies
+    dependencies            = settings_json('requirements', 'dependencies')
+except:
+    pass
