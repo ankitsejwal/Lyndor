6b8442d7a319dfcd3548fcdb0520ef9a72504c43
William Grant <willmgrant@gmail.com>
2018-10-27 07:14:27 +0200

    made chapter formatting code into its own method since it was duplicated

--- module/save.py
+++ module/save.py
@@ -154,6 +154,31 @@ def info_file(url, course_path):
     # print message
     message.print_line(message.INFO_FILE_CREATED)
 
+def format_chapter(chapter, chapter_count):
+    # handle empty named chapters
+    if len(chapter) == 0:
+        chapter = "Unnamed"
+
+    # Check for valid characters
+    replacements = [
+        ('[?]', ''),
+        ('[/]', '_'),
+        ('["]', "'"),
+        ('[:><\\|*]', ' -')
+    ]
+
+    for old, new in replacements:
+        chapter = re.sub(old, new, chapter)                
+
+    if chapter[1] == '.':
+        chapter = str(chapter_count).zfill(2) + '. ' + chapter[3:]
+    elif chapter[2] == '.':
+        chapter = str(chapter_count).zfill(2) + '. ' + chapter[4:]
+    else:
+        chapter = str(chapter_count).zfill(2) + '. ' + chapter
+
+    return chapter
+
 def chapters(url, course_folder_path):
     ''' create chapters folder '''
     soup = create_soup(url)
@@ -164,30 +189,7 @@ def chapters(url, course_folder_path):
 
     
     for h in heading4:
-        chapter = h.text
-        
-        # handle empty named chapters
-        if len(chapter) == 0:
-            chapter = "Unnamed"
-    
-        # Check for valid characters
-        replacements = [
-            ('[?]', ''),
-            ('[/]', '_'),
-            ('["]', "'"),
-            ('[:><\\|*]', ' -')
-        ]
-
-        for old, new in replacements:
-            chapter = re.sub(old, new, chapter)                
-
-        if chapter[1] == '.':
-            chapter = str(chapter_no).zfill(2) + '. ' + chapter[3:]
-        elif chapter[2] == '.':
-            chapter = str(chapter_no).zfill(2) + '. ' + chapter[4:]
-        else:
-            chapter = str(chapter_no).zfill(2) + '. ' + chapter
-        
+        chapter = format_chapter(h.text, chapter_no)    
         chapter_no += 1
         message.print_line(chapter)
 
@@ -196,7 +198,7 @@ def chapters(url, course_folder_path):
         os.mkdir(new_chapter) # create folders (chapters)
             
     message.colored_message(Fore.LIGHTGREEN_EX, '\n✅  '+str(chapter_no)+' chapters created!!\n')
- 
+
 def contentmd(url):
     ''' write chapters and videos information to content.md '''
 
@@ -214,32 +216,9 @@ def contentmd(url):
     bug = False
     for li in ul_video:
         try:
-            chapter = chapters[chapter_count].text
-            
-            # handle empty named chapters
-            if len(chapter) == 0:
-                chapter = "Unnamed"
-
-            # Check for valid characters
-            replacements = [
-                ('[?]', ''),
-                ('[/]', '_'),
-                ('["]', "'"),
-                ('[:><\\|*]', ' -')
-            ]
-
-            for old, new in replacements:
-                chapter_title = re.sub(old, new, chapter)                
-
-            if chapter[1] == '.':
-                chapter_title = str(chapter_count).zfill(2) + '. ' + chapter[3:]
-            elif chapter[2] == '.':
-                chapter_title = str(chapter_count).zfill(2) + '. ' + chapter[4:]
-            else:
-                chapter_title = str(chapter_count).zfill(2) + '. ' + chapter
-            
-            chapter_name = u'\n\n## {}\n'.format(chapter)
-            content_md.writelines(chapter_name)
+            chapter = format_chapter(chapters[chapter_count].text, chapter_count) 
+            chapter_markdown = u'\n\n## {}\n'.format(chapter)
+            content_md.writelines(chapter_markdown)
         except Exception:
             bug = True
             break              
@@ -250,7 +229,7 @@ def contentmd(url):
             video_count += 1
             try:
                 video_name = u"{} - {}".format(str(video_count).zfill(2), video.text.strip())
-                video_markdown = "\n* [" + video_name + "](" + chapter_title + "/" + video_name + ".mp4)"
+                video_markdown = "\n* [" + video_name + "](" + chapter + "/" + video_name + ".mp4)"
                 content_md.writelines(video_markdown)
             except Exception:
                 bug = True
